5d8b05f574e78b899f288d030efd21ba286844dc
wangjie3@shanghai3h.com <wangjie3@shanghai3h.com>
2022-04-26 19:47:12 +0800

    提示

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,13 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             string a = "test01111";
-
-=======
             string a = "test";
-            string b = "test";
->>>>>>> 1c4cd6f40e689340d0d931050847f518067bad73
         }
     }
 }
